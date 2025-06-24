c3a3bc73a9085fea69d92dd22b634b0c8b9e36eb
Anchal <anchalaadiwal12@gmail.com>
2025-06-24 22:32:40 +0530

    model changes done

--- Models/Contact.Model.js
+++ Models/Contact.Model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const ContactSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    mobileNumber: { type: String, required: true },
+    Number: { type: String, required: true },
     email: { type: String },
     message: { type: String, required: true }
 }, {
